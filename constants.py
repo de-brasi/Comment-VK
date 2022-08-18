5673d3ee1bd4fda31425aa8c1ac05f3e8f45ec1f
ilya <local>
2022-08-18 23:25:30 +0300

    Add constants.py file

---
+++ constants.py
@@ -0,0 +1,16 @@
+FONT = ('Georgia', 10)
+TITLE = "ВК-комментарий по таймеру"
+GEOMETRY = "440x330"
+
+TIME_INVITATION = "Время (час/мин/сек) - "
+TIME_ENTER_BUTTON_TEXT = "ввести"
+
+PHOTO_COUNT_TEXT = "Добавлено фото:"
+
+ADD_PHOTO_TEXT = "Добавить к списку"
+DEL_PHOTO_TEXT = "Удалить из списка"
+
+TIME_DEFAULT_HOUR = 12
+TIME_DEFAULT_MINUTE = 0
+TIME_DEFAULT_SECOND = 0
+
